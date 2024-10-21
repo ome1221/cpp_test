edf3eccaad729a3e9700331920945836860f67dd
ome1221 <ome0151@gmail.com>
2024-10-21 12:16:39 +0530

    Create ABS.cpp

---
+++ ABS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// Macro to calculate absolute value
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+int main() {
+    int num = -10;
+    int absoluteValue = ABS(num);
+
+    cout << "Absolute value of " << num << " is: " << absoluteValue << endl;
+
+    return 0;
+}
