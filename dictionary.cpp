5d09c3bf2fcdd11d8e65be76d63b1f62ff477543
ome1221 <ome0151@gmail.com>
2024-10-24 22:31:50 +0530

    Update dictionary.cpp

--- dictionary.cpp
+++ dictionary.cpp
@@ -2,41 +2,35 @@
 #include <vector>
 using namespace std;
 
-// Custom Map class to store key-value pairs
 template<typename K, typename V>
-class MyMap {
+class Dictionary {
 private:
-    vector<pair<K, V>> items; // Vector of pairs to hold key-value pairs
+    vector<pair<K, V>> items;
 
 public:
-    // Function to add key-value pairs
     void add(K key, V value) {
         for (auto& item : items) {
             if (item.first == key) {
-                item.second = value; // Update if key already exists
+                item.second = value;
                 return;
             }
         }
-        // Add new key-value pair if key does not exist
         items.push_back(make_pair(key, value));
     }
 
-    // Operator overload for accessing elements using []
     V& operator[](K key) {
         for (auto& item : items) {
             if (item.first == key) {
-                return item.second; // Return reference to the value
+                return item.second;
             }
         }
-        // If the key does not exist, insert it with a default value and return the reference
         items.push_back(make_pair(key, V()));
         return items.back().second;
     }
 
-    // Friend function to overload the << operator
-    friend ostream& operator<<(ostream& os, const MyMap& myMap) {
+    friend ostream& operator<<(ostream& os, const Dictionary& dictionary) {
         os << "Key : Value" << endl;
-        for (const auto& item : myMap.items) {
+        for (const auto& item : dictionary.items) {
             os << item.first << " : " << item.second << endl;
         }
         return os;
@@ -44,21 +38,16 @@ public:
 };
 
 int main() {
-    // Create an instance of MyMap with int keys and string values
-    MyMap<int, string> deviceCodeMap;
+    Dictionary<int, string> deviceCodeMap;
 
-    // Add key-value pairs
     deviceCodeMap.add(1, "D1");
     deviceCodeMap.add(2, "D2");
     deviceCodeMap.add(3, "D3");
     deviceCodeMap.add(4, "D4");
 
-    // Access a specific value by key using the overloaded [] operator
     string deviceName = deviceCodeMap[2];
-    cout << "Accessed device: " << deviceName << endl; // Output: D2
+    cout << deviceName << endl;
 
-    // Output all key-value pairs using the overloaded << operator
-    cout << "\nDump all key-value pairs:" << endl;
     cout << deviceCodeMap;
 
     return 0;
